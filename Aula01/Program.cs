852bc3e55d413736e95b8b7411668768ef503420
Luis Felipe <luis@witetec.com>
2026-06-22 10:39:19 -0300

    aula 02 - sintaxe basica

---
+++ Aula01/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Aula01
+{
+   public class Program
+    {
+       public static void Main(string[] args)
+        {
+            Console.WriteLine("Iniciando no .NET!");
+        }
+    }
+}
